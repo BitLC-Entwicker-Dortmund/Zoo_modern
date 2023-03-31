d61b7114347c4abd5254c1ab845f26198ad2b4b0
Klaus <BitLCEntwicklerDortmund@web.de>
2023-03-31 08:18:34 +0200

    Add project files.

---
+++ Zoo_modern/Lieferant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo_modern {
+    internal class Lieferant {
+        private string name;
+
+        public void SetName(string n) {
+            name = n;
+        }
+
+        public string GetName() {
+            return name;
+        }
+
+        public Lieferant(string name) {
+            this.name = name;
+        }
+
+        public Rechnung RechnungErstellen() {
+            return new Rechnung();
+        }
+    }
+}
---
+++ Zoo_modern/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Zoo_modern {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, Zoo!");
+
+            Lager lager = new Lager();
+            List<Tier> tiere = new List<Tier> ();
+            Tierpfleger tschackeline = new Tierpfleger();
+            Tierpfleger kevin = new Tierpfleger();
+            Verwaltung verwaltung = new Verwaltung();
+            Erlebniswelt alaska = new Erlebniswelt();
+            Erlebniswelt asien = new Erlebniswelt();
+            Erlebniswelt afrika = new Erlebniswelt();
+            FutterverwaltungsSystem fvs = new FutterverwaltungsSystem();
+
+            Lieferant l1 = new Lieferant("Toni");
+
+
+
+        }
+    }
+}
